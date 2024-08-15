96817569674b3bdda35d185fc7c264aaeb842d84
Mohammad Al Amin <alaminbamna08@gmail.com>
2024-08-15 11:38:08 +0600

    generateBookingId

--- psh-server/utils/generateBookingId.js
+++ psh-server/utils/generateBookingId.js
@@ -1,19 +1,29 @@
 import OrderModel from "../models/Order.js";
-
 const findLastBookingId = async () => {
-  const lastBooking = await OrderModel.findOne({}, { companyId: 1, _id: 0 })
+  const lastBookingId = await OrderModel.findOne({}, { bookingId: 1, _id: 0 })
     .sort({
       createdAt: -1,
     })
     .lean();
 
-  return lastBooking?.bookingId;
+  return lastBookingId?.bookingId;
 };
 
-export const generateBookingId = async (name) => {
-  const currentId =
-    (await findLastBookingId()) || (0).toString().padStart(5, "0"); //00000
-  //increment by 1
-  const incrementedId = (parseInt(currentId) + 1).toString().padStart(5, "0");
-  return incrementedId;
+export const generateBookingId = async () => {
+  const date = new Date();
+  const year = date.getFullYear().toString().substring(2, 4);
+
+  let previousId = await findLastBookingId();
+  let currentYear = year;
+
+  if (previousId && previousId.startsWith(currentYear)) {
+    let convertNumberPreviousId = previousId.slice(2);
+    let incrementedId = (parseInt(convertNumberPreviousId) + 1)
+      .toString()
+      .padStart(4, "0");
+    return currentYear + incrementedId;
+  } else {
+    // Reset to "0001" for the new year
+    return currentYear + "0001";
+  }
 };
