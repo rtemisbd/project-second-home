6b7aed12540b064a9fa7b54cd7a76f126e31cff3
Mohammad Al Amin <alaminbamna08@gmail.com>
2024-09-02 16:39:39 +0600

    some update

--- psh-admin/src/pages/Transaction/UpdateTransaction.jsx
+++ psh-admin/src/pages/Transaction/UpdateTransaction.jsx
@@ -305,6 +305,7 @@ const UpdateTransaction = ({
                       <option value="June">June</option>
                       <option value="July">July</option>
                       <option value="August">August</option>
+                      <option value="September">September</option>
                       <option value="October">October</option>
                       <option value="November">November</option>
                       <option value="December">December</option>
