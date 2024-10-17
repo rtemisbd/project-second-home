b40477551b7a1e97c2bbad99755e9e4b358ea7f9
Mohammad Al Amin <alaminbamna08@gmail.com>
2024-10-17 18:39:33 +0600

    some error solved

--- psh-admin/src/hooks/useTransaction.js
+++ psh-admin/src/hooks/useTransaction.js
@@ -1,11 +1,22 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
+import { getFromLocalStorage } from "../utils/local-storage";
+import { authKey } from "../utils/storageKey";
 
 const useTransaction = () => {
   const [transactions, setTransaction] = useState([]);
+  // Get the access token
+  const accessToken = getFromLocalStorage(authKey);
+
+  // Set the headers
+  const headers = {
+    Authorization: `${accessToken}`,
+    "Content-Type": "application/json",
+  };
   const { isLoading, refetch } = useQuery([transactions], () =>
     fetch(`https://api.psh.com.bd/api/transaction`, {
       method: "GET",
+      headers: headers,
     })
       .then((res) => res.json())
       .then((data) => {
