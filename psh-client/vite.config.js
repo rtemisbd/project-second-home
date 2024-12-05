2908cdcda4e780175ace4cc15f9f82162e306342
Mohammad Al Amin <alaminbamna08@gmail.com>
2024-12-05 11:12:08 +0600

    vite.config file update

--- psh-client/vite.config.js
+++ psh-client/vite.config.js
@@ -10,7 +10,9 @@ export default defineConfig({
   optimizeDeps: {
     include: ["react-to-print", "splidejs/react-splide"], // Ensure this dependency is pre-bundled
   },
-
+  define: {
+    "process.env": process.env,
+  },
   // server: {
   //   proxy: {
   //     "/api": {
