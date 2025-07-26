2da70072aa8e54274f8621a7b1e3e7893a881c94
rakibakter <rakibaakter635@gmail.com>
2025-07-26 17:48:30 +0600

    added password change form

--- psh-admin/src/pages/resort-admin/account/MyAccount.jsx
+++ psh-admin/src/pages/resort-admin/account/MyAccount.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 import { AuthContext } from "../../../contexts/UserProvider";
 import axios from "axios";
 import { baseUrl } from "../../../utils/getBaseURL";
@@ -7,10 +7,66 @@ import { getFromLocalStorage } from "../../../utils/local-storage";
 import { authKey } from "../../../utils/storageKey";
 import img3 from "../../../img/home/profile.png";
 import { formatDate } from "../../../utils/dateConvert";
+import { Modal } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const MyAccount = () => {
   const { user } = useContext(AuthContext);
   const [userData, setUserData] = useState(user);
+  const [reFetch, setReFetch] = useState(false);
+  const [showPasswordModal, setShowPasswordModal] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+    watch,
+  } = useForm();
+
+  const handleUpdateUser = async (e) => {
+    try {
+      e.preventDefault();
+      const payload = {
+        firstName: e.target.firstName.value,
+        phone: e.target.phone.value,
+      };
+      await axios.patch(`${baseUrl}/api/users/${user?._id}`, payload);
+      setReFetch(true);
+      toast.success("Your account updated successfully!");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handlePasswordChange = async (data) => {
+    const { newPassword, confirmPassword } = data;
+
+    if (newPassword !== confirmPassword) {
+      toast.error("Passwords do not match");
+      return;
+    }
+
+    try {
+      const accessToken = getFromLocalStorage(authKey);
+      const headers = {
+        Authorization: `${accessToken}`,
+        "Content-Type": "application/json",
+      };
+
+      await axios.patch(
+        `${baseUrl}/api/users/${user?._id}/update-password`,
+        { password: newPassword },
+        { headers }
+      );
+
+      toast.success("Password updated successfully!");
+      setShowPasswordModal(false);
+      reset(); // Reset form after success
+    } catch (error) {
+      console.error(error);
+      toast.error("Password update failed");
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,13 +83,14 @@ const MyAccount = () => {
           headers,
         });
         setUserData(data?.data);
+        setReFetch(false);
       } catch (error) {
         console.log(error);
       }
     };
 
     fetchData();
-  }, [user?._id]);
+  }, [user?._id, reFetch]);
 
   console.log(userData);
 
@@ -80,7 +137,11 @@ const MyAccount = () => {
                 </div>
               </div>
               <div className="d-flex justify-content-end pr-5">
-                <button type="button" className="btn">
+                <button
+                  onClick={() => setShowPasswordModal(!showPasswordModal)}
+                  type="button"
+                  className="btn text-success btn-link"
+                >
                   Change Password
                 </button>
               </div>
@@ -93,7 +154,10 @@ const MyAccount = () => {
                 borderRadius: "12px",
               }}
             >
-              <form style={{ width: "100%" }}>
+              <form
+                onSubmit={(e) => handleUpdateUser(e)}
+                style={{ width: "100%" }}
+              >
                 <div className="d-flex gap-3">
                   <div style={{ width: "50%" }}>
                     <label>Full Name</label>
@@ -113,6 +177,7 @@ const MyAccount = () => {
                       defaultValue={userData?.email}
                       className="main_form w-100 p-2 "
                       name="email"
+                      disabled
                     />
                   </div>
                 </div>
@@ -131,7 +196,7 @@ const MyAccount = () => {
                     <label>Role</label>
                     <br />
                     <input
-                      type="email"
+                      type="text"
                       defaultValue={userData?.role}
                       className="main_form w-100 p-2 "
                       name="role"
@@ -150,6 +215,72 @@ const MyAccount = () => {
             </div>
           </div>
         </section>
+        {showPasswordModal && (
+          <Modal
+            show={showPasswordModal}
+            onHide={() => {
+              setShowPasswordModal(false);
+            }}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>Set New Password</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <form onSubmit={handleSubmit(handlePasswordChange)}>
+                <div className="mb-3">
+                  <label>New Password</label>
+                  <input
+                    type="password"
+                    {...register("newPassword", {
+                      required: "New Password is required",
+                      pattern: {
+                        value:
+                          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?#&^])[A-Za-z\d@$!%*?#&^]{8,}$/,
+                        message:
+                          "Password must be at least 8 characters and include uppercase, lowercase, number, and special character",
+                      },
+                    })}
+                    className="main_form w-100 px-2"
+                    placeholder="Type New Password"
+                  />
+                  {errors.newPassword && (
+                    <small className="text-danger">
+                      {errors.newPassword.message}
+                    </small>
+                  )}
+                </div>
+                <div className="mb-3">
+                  <label>Confirm New Password</label>
+                  <input
+                    type="password"
+                    {...register("confirmPassword", {
+                      required: "Confirm Password is required",
+                      validate: (value) =>
+                        value === watch("newPassword") ||
+                        "Passwords do not match",
+                    })}
+                    className="main_form w-100 px-2"
+                    placeholder="Confirm New Password"
+                  />
+                  {errors.confirmPassword && (
+                    <small className="text-danger">
+                      {errors.confirmPassword.message}
+                    </small>
+                  )}
+                </div>
+                <div className="d-flex justify-content-center">
+                  <button
+                    type="submit"
+                    className="btn btn-success"
+                    style={{ width: 220 }}
+                  >
+                    Update Password
+                  </button>
+                </div>
+              </form>
+            </Modal.Body>
+          </Modal>
+        )}
 
         <Toaster
           containerStyle={{ top: 300 }}
--- psh-server/services/auth.service.js
+++ psh-server/services/auth.service.js
@@ -5,54 +5,53 @@ import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-const loginUserWithJWT = async(payload)=>{
-    const { phone, password } = payload;
+const loginUserWithJWT = async (payload) => {
+  const { phone, password } = payload;
 
-    // Find the user by phone and populate the branch field
-        const user = await User.findOne({ phone }).populate("branch");
-        // console.log({user});
-        
-    
-        // If the user does not exist, return an error message
-        if (!user) {
-            // console.log(1);
-           throw new AppError(404, "User Not Found!");
-        }
-        if (user.userStatus === "Blocked" || user.userStatus === "Deactive") {
-            // console.log(2);
-            throw new AppError(204 , "User is blocked or deactivated and cannot log in");
-        }
-    
-        // Compare the provided password with the stored password
-        const passwordMatch = await bcrypt.compare(password, user.password);
-     
-        // If the passwords do not match, return an error message
-        if (!passwordMatch) {
-            // console.log(3);
-            throw new AppError(406, "Invalid password. Please try again!");
-        }
-    
-        // Create a user object with limited properties, including the branch
-        const userData = {
-          _id: user._id,
-          branch: user?.branch,
-          firstName: user?.firstName,
-          lastName: user?.lastName,
-          email: user?.email,
-          phone: user?.phone,
-          role: user?.role,
-        };
-    
-        // Generate a JWT token
-        const token = jwt.sign(
-          { userId: user._id, role: user?.role },
-          config.jwt.secret
-        );
-    
-        // Return the token and user information
-        return{ token, user: userData };
-}
+  // Find the user by phone and populate the branch field
+  const user = await User.findOne({ phone }).populate("branch");
+  // console.log({user});
+
+  // If the user does not exist, return an error message
+  if (!user) {
+    // console.log(1);
+    throw new AppError(404, "User Not Found!");
+  }
+  if (user.userStatus === "Blocked" || user.userStatus === "Deactive") {
+    // console.log(2);
+    throw new AppError(204, "User is blocked or deactivated and cannot log in");
+  }
+
+  // Compare the provided password with the stored password
+  const passwordMatch = await bcrypt.compare(password, user.password);
+
+  // If the passwords do not match, return an error message
+  if (!passwordMatch) {
+    // console.log(3);
+    throw new AppError(406, "Invalid password. Please try again!");
+  }
+
+  // Create a user object with limited properties, including the branch
+  const userData = {
+    _id: user._id,
+    branch: user?.branch,
+    firstName: user?.firstName,
+    lastName: user?.lastName,
+    email: user?.email,
+    phone: user?.phone,
+    role: user?.role,
+  };
+
+  // Generate a JWT token
+  const token = jwt.sign(
+    { userId: user._id, role: user?.role },
+    config.jwt.secret
+  );
+
+  // Return the token and user information
+  return { token, user: userData };
+};
 
 export const authServices = {
-    loginUserWithJWT
-}
+  loginUserWithJWT,
+};
